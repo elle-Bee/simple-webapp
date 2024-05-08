19f1d19e5c1cfe42095879512bf7ba4bf6b25c5e
elle-Bee <65856801+elle-Bee@users.noreply.github.com>
2024-05-08 14:10:28 +0530

    docker container up

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM golang:latest
+
+WORKDIR /app
+
+COPY go.mod ./
+COPY go.sum ./
+RUN go mod download
+
+COPY . .
+#RUN go build -v -o /usr/local/bin/app ./...
+
+RUN go build -o main .
+
+EXPOSE 8080
+
+#RUN chmod +x main
+
+CMD ["./main"]
--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"html/template"
 	"net/http"
-	"os"
-
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 type Person struct {
@@ -17,62 +11,20 @@ type Person struct {
 }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("Error loading .env file")
-	}
-
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	uname := os.Getenv("MYSQL_USERNAME")
-	pass := os.Getenv("MYSQL_PASSWORD")
-	dbname := os.Getenv("MYSQL_DB")
-
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s", uname, pass, dbname))
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer db.Close()
-
-	rows, err := db.Query("SELECT * FROM users")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var people []Person
-
-	for rows.Next() {
-		var p Person
-		if err := rows.Scan(&p.ID, &p.Name); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		people = append(people, p)
-	}
-	if err := rows.Err(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 	tmpl, err := template.New("index").Parse(`
         <!DOCTYPE html>
         <html>
         <head>
-            <title>People</title>
+            <title>Sample Web App</title>
         </head>
         <body>
-            <h1>People</h1>
-            <ul>
-                {{range .}}
-                    <li>{{.ID}}: {{.Name}}</li>
-                {{end}}
-            </ul>
+            <h1>Sample Web App</h1>
         </body>
         </html>
     `)
@@ -82,7 +34,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Execute the template and send the response
-	err = tmpl.Execute(w, people)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
